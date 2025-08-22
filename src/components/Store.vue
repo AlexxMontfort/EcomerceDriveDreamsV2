364a98407b8da544c216eb4c5c6b253d1bec7400
AlexxMontfort <alex.gzmm@gmail.com>
2025-08-22 13:30:57 -0600

    Se agrega la tarjeta plantilla de store

--- src/components/Store.vue
+++ src/components/Store.vue
@@ -1,24 +1,35 @@
 <template>
     <section class="py-12 scroll-mt-20 bg-gray-500">
         <main class="container mx-auto px-4 bg-white">
-            <ul>
-                <li>
+            <ul class="grid grid-cols-4">
+                <li class="grid grid-row-2 m-4 rounded-lg p-3 shadow-xl bg-white hover:scale-105 transition-all transition-300">
+                    <figure>
+                        <img src="../assets/centra2026product.png" alt="">
+                    </figure>
                     <article>
-                        <figure>
-                            <img src="../assets/centra2026product.png" alt="">
-                            <figcaption>
-
-                            </figcaption>
-                        </figure>
-                        <section>
-                            <h3>name</h3>
-                            <h4>marca</h4>
-                            <footer>
-                                <div>
-                                    <p>$$$$</p>
-                                </div>
-                            </footer>
-                        </section>
+                        <header>
+                            <h2 class="font-semibold font-3xl">AUTO: Centra</h2>
+                            <p class="font-semibold ">AÑO: 2025</p>
+                            <p class="font-semibold">MARCA: Nissan</p>
+                        </header>
+                        <footer>
+                            <p class="font-semibold">PRECIO: $$$$$$</p>
+                        </footer>
+                    </article>
+                </li>
+                <li class="grid grid-row-2 m-4 rounded-lg p-3 shadow-xl bg-white">
+                    <figure>
+                        <img src="../assets/centra2026product.png" alt="">
+                    </figure>
+                    <article>
+                        <header>
+                            <h2 class="font-semibold font-3xl">AUTO: Centra</h2>
+                            <p class="font-semibold ">AÑO: 2025</p>
+                            <p class="font-semibold">MARCA: Nissan</p>
+                        </header>
+                        <footer>
+                            <p class="font-semibold">PRECIO: $$$$$$</p>
+                        </footer>
                     </article>
                 </li>
             </ul>
